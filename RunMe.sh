3c4cb88245052cf7981dde1eff37def37e75fa30
Razvan D <razvan@dinica.ro>
2024-02-24 18:03:08 +0200

    adding installation shell scripts

---
+++ RunMe.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+### Get absolute path of the current directory
+DOT_FILES_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+rm -rf ${HOME}/.config/tmux
+rm -rf ${HOME}/.tmux.conf
+mkdir ${HOME}/.config/tmux
+mkdir ${HOME}/.config/tmux/plugins
+ln -s ${DOT_FILES_PATH}/tmux/conf ${HOME}/.tmux.conf
+
+rm -rf ${HOME}/.config/kitty
+ln -s ${DOT_FILES_PATH}/kitty ${HOME}/.config/kitty
+
+rm -rf $HOME/.config/nvim
+ln -s ${DOT_FILES_PATH}/neovim ${HOME}/.config/nvim
