35ccbf355cb8249e54550ee82eec081de5368a4b
Razvan D <razvan@dinica.ro>
2024-11-15 01:37:56 +0200

    adding a script to remove path duplicates

---
+++ RemovePathDuplicates.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# 1. echo "$PATH"      - Outputs the current PATH.
+# 2. tr ':' '\n'       - Converts colons to newlines.
+# 3. awk '!seen[$0]++' - Keeps track of seen entries and only prints unique ones.
+# 4. tr '\n' ':'       - Back to colon-separated format.
+# 5. sed 's/:$//'      - Removes the trailing colon.
+
+export PATH=$(echo "$PATH" | tr ':' '\n' | awk '!seen[$0]++' | tr '\n' ':' | sed 's/:$//')
+exit 0
--- RunMe.sh
+++ RunMe.sh
@@ -1,20 +1,38 @@
 #!/usr/bin/env bash
 
-### Get absolute path of the current directory
+if [ "${DEBUG}" == "true" ]; then
+  set -x
+fi
+
+# Get absolute path of the current directory.
 DOT_FILES_PATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-rm -rf ${HOME}/.config/tmux
-rm -rf ${HOME}/.tmux.conf
-mkdir ${HOME}/.config/tmux
-mkdir ${HOME}/.config/tmux/plugins
-ln -s ${DOT_FILES_PATH}/tmux/conf ${HOME}/.tmux.conf
+# Handle symlinks for tmux config directories.
+TMUX_CONFIG_PATH="${HOME}/.config/tmux"
+rm -rf $TMUX_CONFIG_PATH
+mkdir -p "${TMUX_CONFIG_PATH}/plugins"
 
-rm -rf ${HOME}/.config/kitty
-ln -s ${DOT_FILES_PATH}/kitty ${HOME}/.config/kitty
+# Handle symlink for tmux config file.
+rm -rf "${HOME}/.tmux.conf"
+ln -s "${DOT_FILES_PATH}/tmux/conf" "${HOME}/.tmux.conf"
 
+# Handle symlinks for kitty editor.
+rm -rf $HOME/.config/kitty
+ln -s $DOT_FILES_PATH/kitty $HOME/.config/kitty
+
+# Handle symlink for neovim.
 rm -rf $HOME/.config/nvim
-ln -s ${DOT_FILES_PATH}/neovim ${HOME}/.config/nvim
+ln -s $DOT_FILES_PATH/neovim $HOME/.config/nvim
 
+# Handle themes using: gnome-shell-extensions and gnome-tweak.
 rm -rf $HOME/.themes/MyTheme
-mkdir -p ${HOME}/.themes/MyTheme/gnome-shell/
-ln -sf ${DOT_FILES_PATH}/gnometheme/gnome-shell.css ${HOME}/.themes/MyTheme/gnome-shell/gnome-shell.css
+mkdir -p $HOME/.themes/MyTheme/gnome-shell/
+ln -sf $DOT_FILES_PATH/gnometheme/gnome-shell.css $HOME/.themes/MyTheme/gnome-shell/gnome-shell.css
+
+# Create symlink into /etc/profile.d/ that points to local ./RemovePathDuplicates.sh.
+if [ -L "/etc/profile.d/RemovePathDuplicates.sh" ]; then
+  sudo rm -rf /etc/profile.d/RemovePathDuplicates.sh
+fi
+sudo ln -s $DOT_FILES_PATH/RemovePathDuplicates.sh /etc/profile.d/
+
+exit 0
